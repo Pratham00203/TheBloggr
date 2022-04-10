7b7ef003d88bdd2d89e7f5a3254090dfe78e30c5
Pratham Shelar <shelarpratham80@gmail.com>
2022-04-10 18:04:42 +0530

    Initialized Frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,10 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+// import { useState } from "react";
 
 export default function App() {
-  const [blogs, setBlogs] = useState([]);
-  const [trendingBlog, setTrendingBlog] = useState({});
-  const [randomBlogs, setRandomBlogs] = useState([]);
-  async function fetchData() {
-    // const config = {
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    // };
-
-    // const body = JSON.stringify({ email, password });
-
-    try {
-      const res = await axios.get("/blogs");
-      setBlogs(res.data.blogs);
-      setTrendingBlog(res.data.trendingBlog);
-      setRandomBlogs(res.data.randomBlogs);
-    } catch (err) {
-      console.log(err.message);
-    }
-  }
-
-  useEffect(() => {
-    fetchData();
-  });
-
+  // const [isAuthenticated, setAuthenticated] = useState(false);
   return (
     <>
-      <div>
-        <h1>Trending Now</h1>
-        <p>{trendingBlog.title}</p>
-        <p>{trendingBlog.createdon}</p>
-      </div>
-
-      <h1>Check Out more</h1>
-      {randomBlogs.map((blog) => {
-        return (
-          <div>
-            <p>{blog.title}</p>
-            <p>{blog.createdon}</p>
-          </div>
-        );
-      })}
-
-      <h1>All Blogs</h1>
-      {blogs.map((blog) => {
-        return (
-          <div>
-            <p>{blog.title}</p>
-            <p>{blog.createdon}</p>
-          </div>
-        );
-      })}
+      <h1>Hello World</h1>
     </>
   );
 }
