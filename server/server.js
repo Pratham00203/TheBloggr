398105025981dd27da92fa39b9c107cdf235cba6
Pratham Shelar <shelarpratham80@gmail.com>
2022-06-12 13:59:46 +0530

    Added Cors Middleware

--- server/server.js
+++ server/server.js
@@ -3,20 +3,15 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const { check, validationResult } = require("express-validator");
+const cors = require("cors");
 
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb',extended : true}));
+app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "*");
-  next();
-});
 
 // Define Routes
 // Using the js files in the routes/api
@@ -29,8 +24,5 @@ app.get("/", (req, res) => {
   res.send("Running");
 });
 
-// app.post("/", (req, res) => {
-//   console.log(req.body);
-// });
 
 app.listen(PORT, () => console.log(`Server Listening at Port ${PORT}`));
